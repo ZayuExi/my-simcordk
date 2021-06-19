736e2fa25310981a67da44d490e2e14a6009ca44
ZayuExi <86171093+ZayuExi@users.noreply.github.com>
2021-06-19 21:59:51 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ README.md
@@ -0,0 +1,15 @@
+# TEXTCORD
+
+[![Documentation](https://godoc.org/github.com/mustansirzia/simcord?status.svg)](http://godoc.org/github.com/MustansirZia/simcord)
+[![Go Report Card](https://goreportcard.com/badge/github.com/MustansirZia/simcord)](https://goreportcard.com/report/github.com/MustansirZia/simcord)
+[![MIT Licence](https://badges.frapsoft.com/os/mit/mit.svg?v=103)](https://opensource.org/licenses/mit-license.php)
+
+<br />
+
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/git/external?repository-url=https%3A%2F%2Fgithub.com%2FMustansirZia%2Fsimcord&env=DISCORD_TOKEN&envDescription=Add%20the%20discord%20token%20from%20the%20previous%20step.&envLink=https%3A%2F%2Fsimcord.now.sh%2Fdiscord%2F&project-name=my-simcord&repository-name=my-simcord)
+
+Serverless function/API to forward SMS messages to a [Discord](https://discordapp.com/) guild text channel. Can also be used to query messages from a Discord guild text channel via SMS.
+
+Written in - Yours truely Golang.
+
+ReadMe coming soon.
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,36 @@
+module.exports = {
+    title: "Textcord",
+    description: "Serverless function/API for sending and receiving messages from a Discord text channel via SMS.",
+    themeConfig: {
+      repo: 'MustansirZia/textcord',
+      repoLabel: 'GitHub',
+      docsDir: 'docs',
+      editLinks: true,
+      editLinkText: 'Help me improve this page!',
+      searchPlaceholder: 'Search Docs...',
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Documentation', link: '/introduction/' }
+      ],
+      sidebar: [
+        {
+            title: 'Simcord Documentation',   
+            collapsable: false, 
+            children: [
+              ['/introduction/', 'Introduction'],
+              ['/discord/', 'Generate Discord Token'],
+              ['/deploy/', 'Go Live!'],
+              ['/sms/', 'SMS Gateway Configuration']
+            ]
+        },
+      ]
+    },
+    plugins: [
+      [
+        '@vuepress/google-analytics',
+        {
+          'ga': 'UA-138243193-2'
+        }
+      ]
+    ]
+  }
